48f47c8072f486365c610e697adacb865a5a8964
Skill Template <template@skill>
2026-05-13 12:51:32 +0800

    Add cache-busting to images, fix IntroSplash image paths, all transparent vials

--- src/sections/VerificationSection.tsx
+++ src/sections/VerificationSection.tsx
@@ -102,7 +102,7 @@ export default function VerificationSection() {
               }}
             />
             <img
-              src="/images/bpc-157.png"
+              src="/images/bpc-157.png?v=2"
               alt="BPC-157 Vial"
               className="relative z-10 w-full transition-transform duration-300"
               style={{
